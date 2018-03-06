b89190857299a19c907a76e56653521c9424920e
sorryDave <sorrydave403@gmail.com>
2018-03-06 12:18:22 +0100

    cerca de la version de producción

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
-from config import config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
-
-from app import routes, models
+from app import routes
