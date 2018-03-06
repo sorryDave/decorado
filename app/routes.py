bf6f6d1fa327da47c4bb2c5bd78e13ded2e4ea4d
sorryDave <sorrydave403@gmail.com>
2018-03-06 12:53:19 +0100

    closer to v1

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 
-@app.route('/')
+#@app.route('/')
 @app.route('/index')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/ejemplos')
+def ejemplos():
+    return render_template('ejemplos.html')
